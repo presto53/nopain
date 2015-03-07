0de39f91a0dbc56dd542610b2f7f9e5ff0a65a27
Pavel Novitsky <altusensix@gmail.com>
2015-03-08 01:13:23 +0300

    indenting

--- lib/api.rb
+++ lib/api.rb
@@ -14,16 +14,16 @@ module NoPaIn
     resource :config do
       desc "Get config(s) for host(s)."
       params do
-        optional :hostname, type: String, desc: "String representing hostname, could be a regexp."
-        optional :tags, type: String, desc: "Space delimited tags"
-        optional :boot, type: String, desc: "Boot enabled/disabled"
-        optional :install, type: String, desc: "Install enabled/disabled"
-        optional :uuid, type: String, desc: "Server UUID"
-        mutually_exclusive :uuid, :hostname
-        mutually_exclusive :uuid, :tags
-        mutually_exclusive :uuid, :boot
-        mutually_exclusive :uuid, :install
-        at_least_one_of :uuid, :hostname, :tags, :boot, :install
+	optional :hostname, type: String, desc: "String representing hostname, could be a regexp."
+	optional :tags, type: String, desc: "Space delimited tags"
+	optional :boot, type: String, desc: "Boot enabled/disabled"
+	optional :install, type: String, desc: "Install enabled/disabled"
+	optional :uuid, type: String, desc: "Server UUID"
+	mutually_exclusive :uuid, :hostname
+	mutually_exclusive :uuid, :tags
+	mutually_exclusive :uuid, :boot
+	mutually_exclusive :uuid, :install
+	at_least_one_of :uuid, :hostname, :tags, :boot, :install
       end
       get do
       end
@@ -42,7 +42,7 @@ module NoPaIn
 	host = NoPain::Host.find_by(uuid: params[:uuid])
 	if host 
 	  update_hwaddrs(host,params)
-          host.checkin = Time.now
+	  host.checkin = Time.now
 	  host.save
 	  host.boot_image ? return_image(host) : status(404)
 	else
@@ -57,13 +57,13 @@ module NoPaIn
 	host.uuid = params[:uuid]
 	host.hostname = Resolv.getname(params[:ip]) rescue 'nopain'
 	host.ip = params[:ip]
-        host.checkin = Time.now
+	host.checkin = Time.now
 	update_hwaddrs(host,params)
 	host.save
       end
 
       def update_hwaddrs(host,params)
-        host.hwaddr = Hash.new unless host.hwaddr
+	host.hwaddr = Hash.new unless host.hwaddr
 	params.each do |key,value|
 	  if /\A(net[0-9]+)_mac\z/ =~ key
 	    host.hwaddr[$1] = value
